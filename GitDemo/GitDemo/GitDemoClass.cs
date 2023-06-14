bfb3e38c6f38ee5f7d9499b33c50aa9537ae7c46
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-06-14 09:21:32 +0530

    [Kiran]Add: // commit message

---
+++ GitDemo/GitDemo/GitDemoClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    public class GitDemoClass
+    {
+        //UC1
+        public void DemoMethod()
+        {
+            Console.WriteLine("Hello mam");
+        }
+
+        //UC2
+        public void DemoMethodUC2()
+        {
+            Console.WriteLine("Hello mam, this is UC2");
+        }
+    }
+}
---
+++ GitDemo/GitDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GitDemoClass gitDemoClass = new GitDemoClass();
+            gitDemoClass.DemoMethod();
+            gitDemoClass.DemoMethodUC2();
+            Console.ReadLine();
+
+        }
+    }
+}
